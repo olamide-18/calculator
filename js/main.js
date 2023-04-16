4b905aafd9772b266feb1f56d641427e06a252c5
olamide <emmanuelegbebi2004@gmail.com>
2023-04-15 19:58:20 -0800

    set to basic calculator

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 let result = 0;
 let calcInput = document.getElementById("calcInput");
+// let data = [];
 
 function enterValue(calcBtnValue){
+    // if(calcBtnValue == "+" || calcBtnValue == "-" || calcBtnValue == "*" || calcBtnValue == "/"){
+    //     data.push(calcInput.value);
+    //     data.push(calcBtnValue);
+    //     console.log(data);
+    // }else{
+    //     if(data[data.length] == "+" || data)
+    //     if(calcInput.value == 0){
+    //         calcInput.value = calcBtnValue;
+    //     }else{
+    //         calcInput.value += calcBtnValue;
+    //     }
+    // }
     if(calcInput.value == 0){
         calcInput.value = calcBtnValue;
     }else{
