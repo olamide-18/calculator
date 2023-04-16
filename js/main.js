4fab9a2da1761db4cb3111cd37ca3b43fd5fc5e8
olamide <emmanuelegbebi2004@gmail.com>
2023-04-15 19:04:53 -0800

    initial upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let result = 0;
+let calcInput = document.getElementById("calcInput");
+
+function enterValue(calcBtnValue){
+    if(calcInput.value == 0){
+        calcInput.value = calcBtnValue;
+    }else{
+        calcInput.value += calcBtnValue;
+    }
+}
+
+function resetValue(){
+    calcInput.value = calcInput.defaultValue;
+}
+
+function negateValue(){
+
+}
+
+function calculateValue(){
+    result = eval(calcInput.value);
+    calcInput.value = result;
+}
+
